d5c1f66ae7472ee573e86e965ce445f4ef82b752
nishan-pradhan06 <nishanpradhan500@gmail.com>
2026-06-29 22:29:02 +0545

    bottom_nav bar

---
+++ lib/features/global_page/bottom_nav_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../../core/widgets/double_press_to_exit.dart';
+import '../home/pages/home_screen.dart';
+
+class BottomNavBarPage extends StatefulWidget {
+  const BottomNavBarPage({super.key});
+  @override
+  State<BottomNavBarPage> createState() => _BottomNavBarPageState();
+}
+
+class _BottomNavBarPageState extends State<BottomNavBarPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    Text('Home'),
+    Text('Home'),
+    Text('Home'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DoubleBackToExit(
+      child: Scaffold(
+        body: IndexedStack(index: _currentIndex, children: _screens),
+        floatingActionButton: ,
+        bottomNavigationBar: Material(
+          elevation: 8,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_rounded),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today_rounded),
+                label: 'Today',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month_rounded),
+                label: 'Monthly',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.report_rounded),
+                label: 'Reports',
+              ),
+            ],
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
