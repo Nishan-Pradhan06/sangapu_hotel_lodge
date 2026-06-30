6e6772af7dd0746a0eb5889374c3c9e855c7bc64
nishan-pradhan06 <nishanpradhan500@gmail.com>
2026-06-30 09:57:07 +0545

    widgets

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,23 +1,6 @@
 import 'package:dio/dio.dart';
-import 'package:get_it/get_it.dart';
-import 'package:lunar_lms/features/auth/blocs/login/log_in_bloc.dart';
-import 'package:lunar_lms/features/auth/cubits/cubit/logout_cubit.dart';
-import 'package:lunar_lms/features/category/blocs/bloc/category_bloc.dart';
-import 'package:lunar_lms/features/category/repository/category_repository.dart';
-import 'package:lunar_lms/features/courses/blocs/courses_list/courses_list_bloc.dart';
-import 'package:lunar_lms/features/courses/blocs/get_my_courses/get_my_courses_bloc.dart';
-import 'package:lunar_lms/features/courses/repository/courses_repository.dart';
-import 'package:lunar_lms/features/gobal_pages/settings/cubits/cubit/theme_appearance_cubit.dart';
-import 'package:lunar_lms/features/profile/blocs/patch_profile/patch_profile_bloc.dart';
-import 'package:lunar_lms/features/profile/blocs/profiles/profile_bloc.dart';
-import 'package:lunar_lms/features/profile/repository/profile_repository.dart';
 import 'package:retry/retry.dart';
 
-import '../../features/auth/blocs/change_password/change_password_bloc.dart';
-import '../../features/auth/blocs/sign_up/sign_up_bloc.dart';
-import '../../features/auth/blocs/verify_email/verify_email_bloc.dart';
-import '../../features/auth/repository/auth_repository.dart';
-import '../../features/gobal_pages/get_started/cubit/getting_starting_cubit.dart';
 import '../network/api_services.dart';
 import '../network/dio_client.dart';
 import '../services/once_cache_service.dart';
