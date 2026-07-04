b0bcee60dc706c0847129db8223e428faf46350e
nishan-pradhan06 <nishanpradhan500@gmail.com>
2026-07-04 10:42:00 +0545

    update api

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Sum
 from .models import ExpenseEntry
 from .serializers import ExpenseEntrySerializer
 
@@ -8,3 +10,27 @@ class ExpenseEntryViewSet(viewsets.ModelViewSet):
     queryset = ExpenseEntry.objects.all().order_by('-created_at')
     serializer_class = ExpenseEntrySerializer
     permission_classes = [IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        
+        latest_entry = queryset.first()
+        total_daily_expenses = 0
+        total_monthly_expenses = 0
+        
+        if latest_entry:
+            current_date = latest_entry.nepali_date
+            current_month = current_date[:7]
+            
+            total_daily_expenses = queryset.filter(nepali_date=current_date).aggregate(Sum('amount'))['amount__sum'] or 0
+            total_monthly_expenses = queryset.filter(nepali_date__startswith=current_month).aggregate(Sum('amount'))['amount__sum'] or 0
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            "summary": {
+                "total_daily_expenses": float(total_daily_expenses),
+                "total_monthly_expenses": float(total_monthly_expenses),
+                "total_records": queryset.count(),
+            },
+            "data": serializer.data
+        })
--- backend/income/views.py
+++ backend/income/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django.db.models import Count, Sum, Q, DecimalField
+from django.db.models import Count, Sum, Q, DecimalField, F
 from django.db.models.functions import Coalesce, Substr
 from rooms.models import RoomEntry
 
@@ -9,21 +9,34 @@ class DailyIncomeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        data = RoomEntry.objects.values('nepali_date').annotate(
+        data = list(RoomEntry.objects.annotate(
+            date=F('nepali_date')
+        ).values('date').annotate(
             total_entries=Count('id'),
             fixed_price_entries=Count('id', filter=Q(fixed_price__isnull=False)),
             custom_price_entries=Count('id', filter=Q(custom_price__isnull=False)),
             total_income=Sum(Coalesce('fixed_price', 0, output_field=DecimalField())) + 
                          Sum(Coalesce('custom_price', 0, output_field=DecimalField()))
-        ).order_by('-nepali_date')
+        ).order_by('-date'))
         
-        return Response(data)
+        total_income = sum((item['total_income'] or 0) for item in data)
+        total_entries = sum((item['total_entries'] or 0) for item in data)
+
+        return Response({
+            "summary": {
+                "total_income": float(total_income),
+                "total_expenses": 0,
+                "net_balance": float(total_income),
+                "total_records": total_entries,
+            },
+            "data": data
+        })
 
 class MonthlyIncomeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        data = RoomEntry.objects.annotate(
+        data = list(RoomEntry.objects.annotate(
             month=Substr('nepali_date', 1, 7)
         ).values('month').annotate(
             total_entries=Count('id'),
@@ -31,6 +44,17 @@ class MonthlyIncomeView(APIView):
             custom_price_entries=Count('id', filter=Q(custom_price__isnull=False)),
             total_income=Sum(Coalesce('fixed_price', 0, output_field=DecimalField())) + 
                          Sum(Coalesce('custom_price', 0, output_field=DecimalField()))
-        ).order_by('-month')
+        ).order_by('-month'))
         
-        return Response(data)
+        total_income = sum((item['total_income'] or 0) for item in data)
+        total_entries = sum((item['total_entries'] or 0) for item in data)
+
+        return Response({
+            "summary": {
+                "total_income": float(total_income),
+                "total_expenses": 0,
+                "net_balance": float(total_income),
+                "total_records": total_entries,
+            },
+            "data": data
+        })
